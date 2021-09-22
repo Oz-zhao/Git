086b22485843642e394acabc31e53a784d6a1c88
dahai <oz.zhao@hotmail.com>
2021-09-22 21:29:28 +0800

    更新1

---
+++ src/main/java/com/zmhk/helloGit.java
@@ -0,0 +1,12 @@
+package com.zmhk;
+
+public class helloGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello,Git");
+        System.out.println("更新1");
+        System.out.println("更新2");
+        System.out.println("更新3");
+    }
+}
